161048a70e836161e465cd5d137d22d01d5836f6
s1n7ax <srineshnisala@gmail.com>
2024-01-16 15:56:01 +0530

    chore: code clean up

--- shell.nix
+++ shell.nix
@@ -1,14 +1,17 @@
 { pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
+let
+  cclib = "${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH";
+  cudalib = "${pkgs.cudaPackages.cuda_cudart.lib}/lib";
+  nvidialib = "${pkgs.linuxPackages.nvidiaPackages.stable}/lib";
+in pkgs.mkShell {
   name = "cuda-env-shell";
   buildInputs = with pkgs; [
     cudaPackages.cuda_nvcc
     cudaPackages.cuda_cudart
+    wget
     (python3.withPackages (py-packages: with py-packages; [ pip setuptools ]))
   ];
   shellHook = ''
-    LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
-    LD_LIBRARY_PATH=${pkgs.cudaPackages.cuda_cudart.lib}/lib:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidiaPackages.stable}/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${cclib}:${nvidialib}:${cudalib}:$LD_LIBRARY_PATH
   '';
 }
